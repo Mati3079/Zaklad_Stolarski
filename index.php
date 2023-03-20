f82c6aa1cfbbcf2b34e6ab7a006355acde6edc8a
Mati3079 <117904542+Mati3079@users.noreply.github.com>
2023-03-20 12:11:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$a=$_POST["imie"];
+$b=$_POST["nazwisko"];
+$c=$_POST["Nr"];
+$d=$_POST["Adres"];
+$e=$_POST["Usluga"];
+
+echo "Imie: ".$a."<br>"." Nazwisko: ".$b."<br>"." Numer telefonu: ".$c."<br>"." Adres e-mail: ".$d."<br>"." Nazwa Usługi".$e."<br>";
+?>
